ada37550f5e2c3fe013f1a849d443181e2defa27
SaurusXI <vermashantanu@hotmail.com>
2020-06-01 13:01:13 +0530

    add first part of insertion - binary tree like insertion

--- source/rb_tree.hpp
+++ source/rb_tree.hpp
@@ -1,3 +1,5 @@
+// --------------------------- DEFINTIONS ---------------------------
+
 enum class Color {
     RED,
     BLACK
@@ -7,12 +9,15 @@ class Node {
     Color color;
     Node* lchild;
     Node* rchild;
+    int val;
+    bool is_null = false;
 
 public:
-    Node() {
+    Node(int val) {
         color = Color::RED;
         lchild = new NullNode();
         rchild = new NullNode();
+        this->val = val;
     }
     
     friend class RB_tree;
@@ -20,6 +25,7 @@ public:
 
 class NullNode: public Node {
     Color color;
+    bool is_null = true;
 
 public:
     NullNode() {
@@ -29,12 +35,64 @@ public:
 
 class RB_tree {
     Node* root;
+    void recursively_insert(int, Node*);
+    bool recursively_validate(Node*);
 
 public:
     RB_tree() {
-        root = new Node();
-        root->color = Color::BLACK;
+        root = new NullNode();
+    }
+    void insert(int);
+    void validate();
+};
+
+
+
+
+// --------------------------- METHODS ---------------------------
+
+
+void RB_tree::insert(int n) {
+    if (root->is_null) {
+            Node* temp = root;
+            root = new Node(n);
+            root->color = Color::BLACK;
+            delete temp;
+        }
+
+    else
+        recursively_insert(n, root);
+}
+
+void RB_tree::recursively_insert(int n, Node* current) {
+    Node* nextnode;                 // Next candidate node for recursion
+    bool stop_here;                   // If selected candidate is null, stop recursing
+    
+    if (n <= current->val) {
+        nextnode = current->lchild;
+        if (current->lchild->is_null)
+            stop_here = true;
+        else
+            stop_here = false;
+    }
+    else {
+        nextnode = current->rchild;
+        if (current->rchild->is_null)
+            stop_here = true;
+        else
+            stop_here = false;
     }
 
 
-};
+    if (stop_here) {
+        Node* temp = nextnode;
+        nextnode = new Node(n);
+        delete temp;
+    }
+    else
+        recursively_insert(n, nextnode);
+}
+
+void RB_tree::validate() {
+    
+}
