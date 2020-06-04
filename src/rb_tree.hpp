ae2e5e3cc0550e7c329e4cd20753a5d7163cacfa
SaurusXI <vermashantanu@hotmail.com>
2020-06-04 17:58:47 +0530

    fix segfault bug

--- src/rb_tree.hpp
+++ src/rb_tree.hpp
@@ -1,247 +1,304 @@
-#include <tuple>
+#ifndef _TREE_
+#define _TREE_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 #include <iostream>
-using namespace std;
+ using namespace std;
 
-enum class Color {
-    RED,
-    BLACK
+#define RED		1
+#define BLACK	2
+ 
+struct node {
+	int key;
+	struct node *left, *right, *p;
+	int color;
 };
-
-enum class ProblemChild {
-    LEFT,
-    RIGHT,
-    NONE
-};
-
-
-// --------------------------- DEFINTIONS ---------------------------
-
-class Node {
-    Color color;
-    Node* lchild;
-    Node* rchild;
-    int val;
-    bool is_null = false;
-
-public:
-    Node() {
-        color = Color::BLACK;
-        is_null = true;
-    }
-    Node(int val) {
-        color = Color::RED;
-        lchild = new Node();
-        rchild = new Node();
-        this->val = val;
-        is_null = false;
-    }
-    
-    friend class RB_tree;
-};
-
-// type containing (is subtree valid, which grandchild is conflicting, subtree head after mutation) 
-typedef tuple<bool, ProblemChild, Node*> validity_info;
-
-class NullNode: public Node {
-    Color color;
-    bool is_null = true;
-
-public:
-    NullNode() {
-        color = Color::BLACK;
-    }
-};
-
-class RB_tree {
-    Node* root;
-    void recursively_insert(int, Node*);
-    validity_info recursively_validate(Node*);
-    Node* color_flip(Node*);
-    Node* rotate(Node*, ProblemChild, ProblemChild);
-
-public:
-    RB_tree() {
-        cout << 'a';
-        root = new Node();
-    }
-    void insert(int);
-    void validate();
-};
-
-
-
-// --------------------------- METHODS ---------------------------
-
-// Mutate
-
-Node* RB_tree::color_flip(Node* current) {
-    current->lchild->color = Color::BLACK;
-    current->rchild->color = Color::BLACK;
-    current->color = Color::RED;
-
-    return current;
+ 
+typedef struct node *_nodeptr;
+struct node NIL;
+_nodeptr NILPTR = &NIL;
+ 
+_nodeptr search(_nodeptr root, int k) {
+	if (root == NILPTR || root->key == k)
+		return root;
+	if (k < root->key)
+		return search(root->left, k);
+	else
+		return search(root->right, k);
+}
+ 
+_nodeptr minimum(_nodeptr root) {
+	while (root->left != NILPTR)
+		root = root->left;
+	return root;
+}
+ 
+_nodeptr maximum(_nodeptr root) {
+	while (root->right != NILPTR)
+		root = root->right;
+	return root;
+}
+ 
+_nodeptr successor(_nodeptr root, int x) {
+	_nodeptr temp = search(root, x);
+	if (temp == NILPTR) {
+		printf("%d not in tree\n", x);
+		return temp;
+	}
+	if (temp->right != NILPTR)
+		return minimum(temp->right);
+	_nodeptr y = temp->p;
+	while (y != NILPTR && temp == y->right) {
+		temp = y;
+		y = y->p;
+	}
+	return y;
+}
+ 
+_nodeptr predecessor(_nodeptr root, int x) {
+	_nodeptr temp = search(root, x);
+	if (temp == NILPTR) {
+		printf("%d not in tree\n", x);
+		return temp;
+	}
+	if (temp->left != NILPTR)
+		return maximum(temp->left);
+	_nodeptr y = temp->p;
+	while (y != NILPTR && temp == y->left) {
+		temp = y;
+		y = y->p;
+	}
+	return y;
 }
 
-Node* RB_tree::rotate(Node* current, ProblemChild problem_child, ProblemChild problem_grandchild) {
-    if (problem_child == ProblemChild::LEFT) {
-        if (problem_grandchild == ProblemChild::LEFT) {
-            Node* head = current->lchild;
-            Node* no_problem_grandchild = head->rchild;
-            
-            current->lchild = no_problem_grandchild;
-            head->rchild = current;
-
-            return head;
-        }
-
-        else if (problem_grandchild == ProblemChild::RIGHT) {
-            Node* head = current->lchild->rchild;
-            Node* problem_child_node = current->lchild;
-
-            Node* temp1 = head->lchild;
-            Node* temp2 = head->rchild;
-
-            head->lchild = problem_child_node;
-            head->rchild = current;
-            problem_child_node->rchild = new Node();
-            current->lchild = new Node();
-
-            delete temp1;
-            delete temp2;
-
-            return head;
-        }
-    }
-
-    else if (problem_child == ProblemChild::RIGHT) {
-        if (problem_grandchild == ProblemChild::LEFT) {
-            Node* head = current->rchild->lchild;
-            Node* problem_child_node = current->rchild;
-
-            Node* temp1 = head->lchild;
-            Node* temp2 = head->rchild;
-
-            head->rchild = problem_child_node;
-            head->lchild = current;
-            current->rchild = new Node();
-            problem_child_node->lchild = new Node();
-
-            delete temp1;
-            delete temp2;
-
-            return head;
-        }
-
-        else if (problem_grandchild == ProblemChild::RIGHT) {
-            Node* head = current->rchild;
-            Node* no_problem_grandchild = head->lchild;
-
-            current->rchild = no_problem_grandchild;
-            head->rchild = current;
-            return head;
-        }
+void display(const string& prefix, _nodeptr current_node, bool is_left) {
+    if (current_node != NILPTR) {
+        cout << prefix;
+        printf((is_left? "├──": "└──"));
+        string c = "black";
+        if(current_node->color == RED) 
+            c = "red";
+        cout << current_node->key << ',' << c << endl;
+        display( prefix + (is_left ? "│   " : "    "), current_node->left, true);
+        display( prefix + (is_left ? "│   " : "    "), current_node->right, false);
     }
 }
 
-// Enforce validity
 
-void RB_tree::validate() {
-    if (root->is_null)
-        return;
-
-    if (!root->lchild->is_null) 
-        recursively_validate(root->lchild);
-    if (!root->rchild->is_null) 
-        recursively_validate(root->rchild);
-
-    if (root->color == Color::RED) 
-        root->color = Color::BLACK;
+void inorder(_nodeptr x) {
+	if (x != NILPTR) {
+		inorder(x->left);
+		printf("%d ", x->key);
+		inorder(x->right);
+	}
+}
+ 
+void leftrotate(_nodeptr *treeroot, _nodeptr x) {
+	_nodeptr y = x->right;
+	x->right = y->left;
+	if (y->left != NILPTR)
+		y->left->p = x;
+	y->p = x->p;
+	if (x->p == NILPTR)
+		*treeroot = y;
+	else if (x->p->left == x)
+		x->p->left = y;
+	else
+		x->p->right = y;
+	y->left = x;
+	x->p = y;
+}
+ 
+void rightrotate(_nodeptr *treeroot, _nodeptr y) {
+	_nodeptr x = y->left;
+	y->left = x->right;
+	if (x->right != NILPTR)
+		x->right->p = y;
+	x->p = y->p;
+	if (y->p == NILPTR)
+		*treeroot = x;
+	else if (y->p->left == y)
+		y->p->left = x;
+	else
+		y->p->right = x;
+	x->right = y;
+	y->p = x;
+}
+ 
+void rbinsertfixup(_nodeptr *treeroot, _nodeptr z) {
+	while (z->p->color == RED) {
+		if (z->p == z->p->p->left) {
+			_nodeptr y = z->p->p->right;
+			if (y->color == RED) {
+				z->p->color = BLACK;
+				y->color = BLACK;
+				z->p->p->color = RED;
+				z = z->p->p;
+			}
+			else {
+				if (z == z->p->right) {
+					z = z->p;
+					leftrotate(treeroot,z);
+				}
+				z->p->color = BLACK;
+				z->p->p->color = RED;
+				rightrotate(treeroot,z->p->p);
+			}
+		}
+		else {
+			_nodeptr y = z->p->p->left;
+			if (y->color == RED) {
+				z->p->color = BLACK;
+				y->color = BLACK;
+				z->p->p->color = RED;
+				z = z->p->p;
+			}
+			else {
+				if (z == z->p->left) {
+					z = z->p;
+					rightrotate(treeroot,z);
+				}
+				z->p->color = BLACK;
+				z->p->p->color = RED;
+				leftrotate(treeroot,z->p->p);
+			}
+		}
+	}
+	(*treeroot)->color = BLACK;
+}
+ 
+void rbinsert(_nodeptr *treeroot, int z) {
+	_nodeptr Z = (_nodeptr) malloc(sizeof(struct node));
+	Z->key = z;
+	_nodeptr y = NILPTR;
+	_nodeptr x = *treeroot;
+	while (x != NILPTR) {
+		y = x;
+		if (Z->key < x->key)
+			x = x->left;
+		else
+			x = x->right;
+	}
+	Z->p = y;
+	if (y == NILPTR)
+		*treeroot = Z;
+	else if (Z->key < y->key)
+		y->left = Z;
+	else
+		y->right = Z;
+	Z->left = NILPTR;
+	Z->right = NILPTR;
+	Z->color = RED;
+	rbinsertfixup(treeroot,Z);
+}
+ 
+void rbtransplant(_nodeptr *treeroot, _nodeptr u, _nodeptr v) {
+	if (u->p == NILPTR)
+		*treeroot = v;
+	else if (u == u->p->left)
+		u->p->left = v;
+	else
+		u->p->right = v;
+	v->p = u->p;
+}
+ 
+void rbdeletefixup(_nodeptr *treeroot, _nodeptr x) {
+	while (x != *treeroot && x->color == BLACK) {
+		if (x == x->p->left) {
+			_nodeptr w = x->p->right;
+			if (w->color == RED) {
+				w->color = BLACK;
+				x->p->color = RED;
+				leftrotate(treeroot,x->p);
+				w = x->p->right;
+			}
+			if (w->left->color == BLACK && w->right->color == BLACK) {
+				w->color = RED;
+				x = x->p;
+			}
+			else {
+			 	if (w->right->color == BLACK) {
+					w->left->color = BLACK;
+					w->color = RED;
+					rightrotate(treeroot,w);
+					w = x->p->right;
+				}
+				w->color = x->p->color;
+				x->p->color = BLACK;
+				w->right->color = BLACK;
+				leftrotate(treeroot,x->p);
+				x = *treeroot;
+			}
+		}
+		else {
+			_nodeptr w = x->p->left;
+			if (w->color == RED) {
+				w->color = BLACK;
+				x->p->color = RED;
+				rightrotate(treeroot,x->p);
+				w = x->p->left;
+			}
+			if (w->left->color == BLACK && w->right->color == BLACK) {
+				w->color = RED;
+				x = x->p;
+			}
+			else {
+				if (w->left->color == BLACK) {
+					w->right->color = BLACK;
+					w->color = RED;
+					leftrotate(treeroot,w);
+					w = x->p->left;
+				}
+				w->color = x->p->color;
+				x->p->color = BLACK;
+				w->left->color = BLACK;
+				rightrotate(treeroot,x->p);
+				x = *treeroot;
+			}
+		}
+	}
+	x->color = BLACK;
+}
+ 
+void rbdelete(_nodeptr *treeroot, int z) {
+	_nodeptr Z = search(*treeroot, z);
+	if (Z == NILPTR) {
+		printf("Node to be deleted not found\n");
+		return;
+	}
+	_nodeptr y = Z;
+	int yoc = y->color;
+	_nodeptr x;
+	if (Z->left == NILPTR) {
+		x = Z->right;
+		rbtransplant(treeroot,Z,Z->right);
+	}
+	else if (Z->right == NILPTR) {
+		x = Z->left;
+		rbtransplant(treeroot,Z,Z->left);
+	}
+	else {
+		y = minimum(Z->right);
+		yoc = y->color;
+		x = y->right;
+		if (y->p == Z)
+			x->p = y;
+		else {
+			rbtransplant(treeroot,y,y->right);
+			y->right = Z->right;
+			y->right->p = y;
+		}
+		rbtransplant(treeroot,Z,y);
+		y->left = Z->left;
+		y->left->p = y;
+		y->color = Z->color;
+	}
+	if (yoc == BLACK)
+		rbdeletefixup(treeroot,x);
 }
 
-validity_info RB_tree::recursively_validate(Node* current) {
-    if (!current->lchild->is_null) {
-        auto l_validity = recursively_validate(current->lchild);
-        current->lchild = get<2>(l_validity); 
+void execute(_nodeptr treeroot) {
 
-        if (!get<0>(l_validity)) {
-            
-            if (current->rchild->color == Color::RED) {
-                current = color_flip(current);
-            }
-            else if(current->rchild->color == Color::BLACK) {
-                current = rotate(current, ProblemChild::LEFT, get<1>(l_validity));
-            }
-        }
-    }
-    if (!current->rchild->is_null) {
-        auto r_validity = recursively_validate(current->rchild);
-        current->rchild = get<2>(r_validity); 
-
-        if (!get<0>(r_validity)) {
-
-            if(current->lchild->color == Color::RED) {
-                current = color_flip(current);
-            }
-            else if (current->lchild->color == Color::BLACK) {
-                current = rotate(current, ProblemChild::RIGHT, get<1>(r_validity));
-            }    
-        }
-    }
-    
-    if (current->color == Color::RED && current->lchild->color == Color::RED) {
-        validity_info info(false, ProblemChild::LEFT, current);
-        return info;
-    }
-    if (current->color == Color::RED && current->rchild->color == Color::RED) {
-        validity_info info(false, ProblemChild::RIGHT, current);
-        return info;
-    }
-
-    validity_info info(true, ProblemChild::NONE, current);
-    return info;
 }
-
-// Insertion
-
-void RB_tree::insert(int n) {
-    if (root->is_null) {
-            Node* temp = root;
-            root = new Node(n);
-            root->color = Color::BLACK;
-            delete temp;
-        }
-
-    else {
-        recursively_insert(n, root);
-        validate();
-    }
-}
-
-void RB_tree::recursively_insert(int n, Node* current) {
-    Node* nextnode;                 // Next candidate node for recursion
-    bool stop_here;                   // If next candidate is null, stop recursing
-    
-    if (n <= current->val) {
-        nextnode = current->lchild;
-        if (current->lchild->is_null)
-            stop_here = true;
-        else
-            stop_here = false;
-    }
-    else {
-        nextnode = current->rchild;
-        if (current->rchild->is_null)
-            stop_here = true;
-        else
-            stop_here = false;
-    }
-
-
-    if (stop_here) {
-        Node* temp = nextnode;
-        nextnode = new Node(n);
-        delete temp;
-    }
-    else
-        recursively_insert(n, nextnode);
-}
+#endif
--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -3,14 +3,77 @@
 using namespace std;
 
 int main() {
-    cout << "Starting program";
-    RB_tree tree;
-    cout << 5;
-    tree.insert(5);
-    cout << 4;
-    tree.insert(4);
-    cout << 3;
-    /*
-    tree.insert(3);
-    */
+	NIL.left = NIL.right = NIL.p = NILPTR;
+	NIL.color = BLACK;
+	_nodeptr tree = NILPTR;
+	int n;
+    printf("\nEnter the expected turnaround time of a process (as would be precomputed by scheduler) to see how this CFS would organize processes.");
+	while (1) {
+		printf("\n1. Insert Process by Execution Time\n2. Search Process by Execution Time\n3. Inorder Walk\n4. Find total execution time of process that will be selected next\n5. Process with maximum execution time\n6. Successor\n7. Predecessor\n8. Delete process from tree\n9. Display current tree\n10. Execute next process\n11. Exit\n");
+		scanf("%d", &n);
+		if (n == 1) {
+			printf("Enter already acquired execution time of incoming process:\n");
+			int num;
+			scanf("%d", &num);
+			rbinsert(&tree, num);
+		}
+		else if (n == 2) {
+			printf("Enter the number to be search\n");
+			int num;
+			scanf("%d", &num);
+			if (search(tree, num) == NILPTR)
+				printf("%d not found\n", num);
+			else
+				printf("%d found\n", num);
+		}
+		else if (n == 3) {
+			inorder(tree);
+			printf("\n");
+		}
+		else if (n == 4)
+			printf("%d\n", minimum(tree)->key);
+		else if (n == 5)
+			printf("%d\n", maximum(tree)->key);
+		else if (n == 6) {
+			printf("Enter the number whose successor needs to be found\n");
+			int num;
+			scanf("%d", &num);
+			_nodeptr t = successor(tree, num);
+			if (t != NULL)
+				printf("%d\n", t->key);
+		}
+		else if (n == 7) {
+			printf("Enter the number whose predecessor needs to be found\n");
+			int num;
+			scanf("%d", &num);
+			_nodeptr t = predecessor(tree, num);
+			if (t != NULL)
+				printf("%d\n", t->key);
+		}
+		else if (n == 8) {
+			printf("Enter the execution time of process to be deleted\n");
+			int num;
+			scanf("%d", &num);
+			rbdelete(&tree, num);
+		}
+        else if (n == 9) {
+            cout << endl;
+            display("", tree, false);
+        }
+		else if (n == 10) {
+			int et;
+			cout << "Enter the amount of time the next process will execute on CPU for\n";
+			cin >> et;
+			int min = minimum(tree)->key;
+			cout << "Executing process with current total execution time " << min << "\n";
+			int new_et = et + min;
+			rbdelete(&tree, min);
+			cout << "The process has finished executing and now has an execution time of " << new_et << "\n";
+			rbinsert(&tree, new_et);
+		}
+		else 
+			break;
+	}
+	return 0;
 }
+ 
